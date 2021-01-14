7c2c345209e8ad57a9f7a4a3746952280b1b3e17
Kulyk Oleksandr <a_kylik@ukr.net>
2021-01-14 16:17:27 +0200

    add

--- src/index.js
+++ src/index.js
@@ -2,16 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Timer from "./timer/timer.js";
+
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+    <Timer
+      autoStart={false}
+      infinity={false}
+      from={10}
+      interval={1000}
+    />
+
+    <Timer
+      autoStart={true}
+      infinity={false}
+      from={99}
+      interval={2000}
+    />
+
+    {/* <Timer
+      autoStart={true}
+      infinity={true}
+      from={60}
+      interval={100}
+    /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
